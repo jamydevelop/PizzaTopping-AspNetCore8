a19d8b781186f4d9120377c50ab6bdbc4c055ccc
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-06-10 22:10:33 +0800

    misc files

--- PizzaToppings/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ PizzaToppings/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6253170db4cbd21980c270f3768431bedf80e6df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2f81a983e9f918295eddc365fb7a9c179a0be61")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
